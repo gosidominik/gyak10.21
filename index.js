bd1e01d7fd59fc8b2545e81be5296f8507242a9d
Dominik <gosidominik@gmail.com>
2020-10-21 21:25:35 +0200

    borders and movement fixed

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const {fillMap} = require('./map');
+const { keyIn } = require('readline-sync');
+const { fillMap, drawMap } = require('./map');
 
-const width = 30;
-const height = 20;
+const width = 20;
+const height = 10;
 
 let map = [];
 const apples = [];
-const player = { pos:{x: 1, y: 1}, score: 0};
+const player = { pos: { x: 1, y: 1 }, score: 0 };
 
-map = fillMap(width, height, player, apples);
+let key;
+do {
+  map = fillMap(width, height, player, apples);
+  drawMap(map);
+
+  key = keyIn();
+  if (key === 's') {
+    player.pos.y++;
+    if (player.pos.y === height - 1) {
+      player.pos.y = height - 2;
+    }
+  } else if (key === 'w') {
+    player.pos.y--;
+    if (player.pos.y === 0) {
+      player.pos.y = 1;
+    }
+  } else if (key === 'a') {
+    player.pos.x--;
+    if (player.pos.x === 0) {
+      player.pos.x = 1;
+    }
+  } else if (key === 'd') {
+    player.pos.x++;
+    if (player.pos.x === width - 1) {
+      player.pos.x = width - 2;
+    }
+  }
+} while (key !== 'q');
--- map.js
+++ map.js
@@ -1,11 +1,37 @@
+const { table, getBorderCharacters } = require('table');
+
 const fillMap = (width, height, player, apples) => {
-    const mapArr = new Array (width);
-    for (let i = 0; i < mapArr.length; i++) {
-        mapArr[i] = new Array (height);
+  const mapArr = new Array(height);
+  for (let i = 0; i < height; i++) {
+    mapArr[i] = new Array(width);
+    for (let j = 0; j < width; j++) {
+      if (i === 0 || j === 0 || i === height - 1 || j === width - 1) {
+        mapArr[i][j] = '+';
+      }
+      if (player.pos.x === j && player.pos.y === i) {
+        mapArr[i][j] = 'x';
+      }
     }
-    return mapArr;
+  }
+  return mapArr;
+};
+
+const drawMap = (map) => {
+  let output = table(map, {
+    border: getBorderCharacters('void'),
+    columnDefault: {
+      paddingLeft: 0,
+      paddingRight: 1
+    },
+    drawHorizontalLine: () => {
+      return false;
+    }
+  });
+  console.clear();
+  console.log(output);
 };
 
 module.exports = {
-    fillMap
+  fillMap,
+  drawMap
 };
